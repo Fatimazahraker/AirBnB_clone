c44156194e4f44dde4c80f13cac52d96e4375644
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-08-08 04:20:17 -0700

    task

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """base class, parent class that child class enherit from it"""
 import models
+import datetime
 from uuid import uuid4
-from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel of the project.
-    define all common attribute of othes classes 
+    """ it is a basemodel calss that define 
+    all attributes and
+    methodes for other class
     """
+    def  __init__(self):
+        """ intialization of the magic init method.
 
-    def __init__(self, *args, **kwargs):
-        """define a class with a magic init method.
-
-        Args:
+        args:
             *args: tuples of argumets.
             **kwargs: dictionary of arguments.
         Attr:
@@ -21,36 +21,32 @@ class BaseModel:
             created_id: created datetime
             updated_at: updated datetime
         """
-        timfrm = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, timfrm)
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
 
     def save(self):
-        """pulic instance methode to save the updated time"""
-        self.updated_at = datetime.today()
-        models.storage.save()
+        """updates the public instance attribute 
+        updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """public instance method return a dictionary
-        Includes the key/value pair __class__ representing
-        the class name of the object.
+        """it a public methode returns a dictionary containing 
+        all keys/values of __dict__ of the instance
         """
-        newdict = self.__dict__.copy()
-        newdict["created_at"] = self.created_at.isoformat()
-        newdict["updated_at"] = self.updated_at.isoformat()
+        newdict = {}
         newdict["__class__"] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if key in ["created_at", "updated_at"]:
+                newdict[key] = value.isoformat()
+            else:
+                newdict[key] = value
         return newdict
 
     def __str__(self):
-        """it is method return print/st representation of the class."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """it is a str format of all instance and its attribute"""
+        classname = self.__class__.__name__
+        return f"{[classname]} {(self.id)} {self.__dict__}"
+
+
