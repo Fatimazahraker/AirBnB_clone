a65def2a6c4197a48ad9a700741ae8f9b04c7774
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-08-11 13:08:45 -0700

    task

--- console.py
+++ console.py
@@ -8,7 +8,7 @@ from models.__init__ import storage
 from models.user import User
 from models.place import Place
 from models.state import State
-from models.city import city
+from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
@@ -155,7 +155,7 @@ def do_update(self, args):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -163,10 +163,10 @@ def do_update(self, args):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 """ molule to store all obejcts """
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """ class that serialize instance to json
@@ -39,7 +45,11 @@ class FileStorage:
         file (__file_path) exists ; otherwise, do nothing. If the 
         file doesn’t exist, no exception should be raised)
         """
-        classes = {'BaseModel': BaseModel}
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                }
 
         try:
             dic = {}
