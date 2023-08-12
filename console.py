ac21bcd5aad968b4705b2010b7c42c001253cd50
ELHEND <alhnd683@gmail.com>
2023-08-12 08:40:05 -0700

    woo

--- console.py
+++ console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Defines  HBnB console."""
 
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 import cmd
 import re
 from shlex import split
@@ -9,9 +12,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
+
 
 
 def parse(arg):
