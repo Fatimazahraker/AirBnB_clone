321e982385b7bd4d73853455b4a790f30281afbe
ELHEND <alhnd683@gmail.com>
2023-08-13 11:20:54 -0700

    apc

--- models/city.py
+++ models/city.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Defines the City class."""
+"""Defines City class."""
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Represent a city.
+    """Represent city.
 
     Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city.
+        state_id (str):  state id.
+        name (str): name of the city.
     """
 
     state_id = ""
     name = ""
+
