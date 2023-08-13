bd4b5e328b4201db4676532ea3940d1443193430
ELHEND <alhnd683@gmail.com>
2023-08-13 08:31:24 -0700

    de

--- models/base_model.py
+++ models/base_model.py
@@ -1,61 +1,51 @@
 #!/usr/bin/python3
-"""base class, parent class that child class enherit from it"""
-
+"""Defines the BaseModel class."""
 import models
-import datetime
-import uuid
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
-    """ it is  basemodel calss that define all attributes and
-    methodes for other class
-    """
+    """Represents the BaseModel of the HBnB project."""
+
     def __init__(self, *args, **kwargs):
-        """ intialization of the magic init method.
+        """Initialize a new BaseModel.
 
-        args:
-            *args: tuples of argumets.
-            **kwargs: dictionary of arguments.
-        Attr:
-            id: unique id by uuid4
-            created_id: created datetime
-            updated_at: updated datetime
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-
-        if len(kwargs) > 0:
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k == "__class__":
-                    pass
-                elif k in ("created_at", "updated_at"):
-                    setattr(self, k, datetime.datetime.fromisoformat(v))
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
-                    setattr(self, k, v)
-        models.storage.new(self)
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
-        """updates public instance attribute
-        updated_at with the current datetime
-        """
-        self.updated_at = datetime.datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """it a public methode returns a dictionary containing
-        all keys/values of __dict__ of the instance
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
         """
-        newdict = {}
-        newdict["__class__"] = self.__class__.__name__
-        for key, value in self.__dict__.items():
-            if key in ["created_at", "updated_at"]:
-                newdict[key] = value.isoformat()
-            else:
-                newdict[key] = value
-        return newdict
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
 
     def __str__(self):
-        """it is a str format of all instance and its attribute"""
-        classname = self.__class__.__name__
-        return f"{[classname]} {(self.id)} {self.__dict__}"
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
