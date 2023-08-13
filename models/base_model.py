57dab90560b4d746d0f3a6e8564a95808b528b15
ELHEND <alhnd683@gmail.com>
2023-08-13 08:35:04 -0700

    qoqo

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+"""Defines  BaseModel class."""
 import models
-from uuid import uuid4
 from datetime import datetime
+from uuid import uuid4
+
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """Represents BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """Initialize  new BaseModel.
 
         Args:
             *args (any): Unused.
@@ -29,12 +30,12 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Update updated_at with current datetime."""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
+        """Return the dictionary of BaseModel instance.
 
         Includes the key/value pair __class__ representing
         the class name of the object.
@@ -46,6 +47,7 @@ class BaseModel:
         return rdict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Return  print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
