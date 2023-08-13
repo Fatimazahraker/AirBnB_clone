2cdc0e193b23a06ba8e1e86d230a425fa89dbca1
ELHEND <alhnd683@gmail.com>
2023-08-13 08:51:44 -0700

    hllow

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""Defines  FileStorage class."""
+from models.amenity import Amenity
+from models.review import Review
 import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
+
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
+    """Represent abstracted storage engine.
 
     Attributes:
-        __file_path (str): The name of the file to save objects to.
+        __file_path (str):  name of  file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return the dictionary __objects."""
+        """Return dictionary __objects."""
         return FileStorage.__objects
 
     def new(self, obj):
@@ -37,13 +38,15 @@ class FileStorage:
             json.dump(objdict, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """Deserialize JSON file __file_path to __objects, if it exists."""
         try:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+                for p in objdict.values():
+                    cls_name = p["__class__"]
+                    del p["__class__"]
+                    self.new(eval(cls_name)(**p))
         except FileNotFoundError:
             return
+
+
